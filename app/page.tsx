665a96a5761556aceaeb6fd1889d29e9ac68a9a6
anshulkamboj05 <anshul.kamboj@edu.sait.ca>
2024-01-16 18:37:39 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ export default  function home(){
   return(
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5x1 w-full items-center justify-between font-mono text-sm 1">
-       <h1> CPRG 306: Web Development 2 - Assignments</h1>
+       <h1 className="text-4xl font-bold mb-5"> CPRG 306: Web Development 2 - Assignments</h1>
       </div>
-      <div>
+      <div className="text-lg">
         <Link href="/week-2">Week-2 Page</Link>
 
      </div>
