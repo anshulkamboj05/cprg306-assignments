773ec456a007b2f6e92691538b4ebc2a6f3ec1c7
Anshul Kamboj <125079825+anshulkamboj05@users.noreply.github.com>
2024-04-10 19:31:44 -0600

    start

--- app/Week-10/layout.js
+++
@@ -1,7 +0,0 @@
-import { AuthContextProvider } from "../Week-10/_utils/auth-context";
- 
-const Layout = ({ children }) => {
-  return <AuthContextProvider>{children}</AuthContextProvider>;
-};
- 
-export default Layout;
